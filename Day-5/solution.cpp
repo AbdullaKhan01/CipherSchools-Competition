1bc76cd776c25dbd9ccd6af7621d0e45efe79ba9
Abdulla Khan <115298492+AbdullaKhan01@users.noreply.github.com>
2023-09-15 20:53:20 +0530

    15th August 2023

--- Day-5/solution.cpp
+++ Day-5/solution.cpp
@@ -1,61 +1,46 @@
-#include<bits/stdc++.h>
-using namespace std;
- 
- struct ListNode {
-      int val;
-      ListNode *next;
-      ListNode() : val(0), next(nullptr) {}
-      ListNode(int x) : val(x), next(nullptr) {}
-      ListNode(int x, ListNode *next) : val(x), next(next) {}
-  };
- 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    int lengthOfList(ListNode* head){
-        int count = 0;
-        while(head!=NULL){
-            head = head->next;
-            count++;
+struct node_cmp
+{
+   bool operator()(ListNode* a,ListNode* b ) 
+   {
+    return a->val > b->val;
+   }
+};
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*,vector<ListNode*>,node_cmp> minHeap;
+        ListNode* head = NULL;
+        ListNode* tail = NULL;
+        for(int i=0;i<lists.size();i++){
+            if(lists[i]!=NULL)
+            minHeap.push(lists[i]);
         }
-        return count;
-    }
-    ListNode* rotateRight(ListNode* head, int k) {
-        if(head == NULL || head -> next == NULL)
-            return head;
-        int size = lengthOfList(head);
-        int rot = k%size;
-        if(rot==0)
-            return head;
-        ListNode* temp = head;
-        ListNode* ans;
-        int l = size - rot - 1;
-        int m = size - rot;
-        int count = 1;
-        while(l--)
-            temp = temp -> next;
-        ListNode* NewHead = temp->next;
-        ListNode* temp2=NewHead;
-        temp -> next = NULL;
-        while(temp2!=NULL && temp2->next!=NULL)
-            temp2=temp2->next;
-        temp2->next = head;
-        return NewHead;
-        // while(temp->next != NULL){
-        //     if(count == m){
-        //         ans = temp->next;
-        //         temp -> next = NULL;
-        //         temp = ans;
-        //         }
-        //     else
-        //         temp = temp->next;
-        //     count++;
-        // }
-        // temp->next = head;
-        // return ans;
+        while(minHeap.size()>0){
+            ListNode* top = minHeap.top();
+            minHeap.pop();
+            if(top->next !=NULL){
+                minHeap.push(top->next);
+            }
+            if(head == NULL){
+                //if inserting at the first node
+                head = top;
+                tail = top;
+            }
+            else{//insert at tail
+                tail -> next = top;
+                tail = top;
+            }
+        }
+        return head;
     }
 };
-int main(){
-     //write here 
-      
-     return 0;
-}
